80d169d8d6c5cc19f17fdcc091e0e8dbf1167d82
charles <48285157+charles0301@users.noreply.github.com>
2019-06-26 11:21:50 +0530

    Update NovalnetServiceProvider.php

--- src/Providers/NovalnetServiceProvider.php
+++ src/Providers/NovalnetServiceProvider.php
@@ -219,7 +219,7 @@ class NovalnetServiceProvider extends ServiceProvider
 			    $account = pluginApp(AccountService::class);
         		    $customerId = $account->getAccountContactId();
 			    if (!empty ($customerId)) {
-			    $log = $transactionLogData->getTransactionData('customerId', $customerId);
+			    //~ $log = $transactionLogData->getTransactionData('customerId', $customerId);
 				    $this->getLogger(__METHOD__)->error('log', $log);
 			    }
 			    			foreach ($address->options as $option) {
